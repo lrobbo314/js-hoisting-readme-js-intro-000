dbbc9d259bcd3c506198f0c530ad33f01637143d
Robert Lego <robertlego@gmail.com>
2018-01-25 06:08:06 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -10,12 +10,14 @@ function callMe() {
 
 function crazy() {
   // fix the code in here:
-  thisIsCrazy();
+  
 
   var thisIsCrazy = function() {
     console.log("hey!!!")
   }
 }
+
+thisIsCrazy();
 
 function sayMyName() {
   // fix the code in here:
